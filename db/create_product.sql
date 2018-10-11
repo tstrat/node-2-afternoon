ba259a3d2f56f67c8268ca94ffe9132497f5b28d
tstrat <tstrat@email.arizona.edu>
2018-10-11 16:57:47 -0700

    Fixes to OBJECT insertion

--- db/create_product.sql
+++ db/create_product.sql
@@ -1,2 +1,2 @@
 INSERT INTO products (name, description, price, image_url)
-VALUES (${name}, ${description}, ${price}, ${image_url});
+VALUES (${name}, ${description}, ${price}, ${image_url}) returning *;
--- db/delete_product.sql
+++ db/delete_product.sql
@@ -1,2 +1,2 @@
 DELETE FROM products
-WHERE product_id = ${id};
+WHERE product_id = ${id} returning *;
